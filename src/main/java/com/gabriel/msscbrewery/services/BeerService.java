aabd3e317b5cbc9acf1f6ee70bfa57bd9a05ea4a
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-07-03 18:59:11 +0200

    create BeerService

---
+++ src/main/java/com/gabriel/msscbrewery/services/BeerService.java
@@ -0,0 +1,9 @@
+package com.gabriel.msscbrewery.services;
+
+import com.gabriel.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+}
