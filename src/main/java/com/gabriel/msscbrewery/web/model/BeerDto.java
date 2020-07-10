5d01d3f6160f2047d7b7b56beafa95ec02c7c3a8
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-07-10 10:50:31 +0200

    upgrade BeerDto

--- src/main/java/com/gabriel/msscbrewery/web/model/BeerDto.java
+++ src/main/java/com/gabriel/msscbrewery/web/model/BeerDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -14,7 +16,14 @@ import java.util.UUID;
 public class BeerDto {
 
     private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
     private String beerName;
-    private String beerStyle;
+    private BeerStyleEnum beerStyle;
     private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
 }
