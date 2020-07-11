673e43e8117ec3de7d562699d980c2865a9a6c76
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-07-11 18:13:36 +0200

    add beer repository

---
+++ src/main/java/com/gabriel/msscbrewery/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.msscbrewery.repositories;
+
+import com.gabriel.msscbrewery.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
