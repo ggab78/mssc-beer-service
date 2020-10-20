46e374e512561e22c371d7f314c0bb326d5cc4f3
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-20 21:13:08 +0200

    correct basic authentication for feign client

--- src/main/java/com/gabriel/beerservice/config/FeignClientSecurityConfig.java
+++ src/main/java/com/gabriel/beerservice/config/FeignClientSecurityConfig.java
@@ -1,18 +1,20 @@
 package com.gabriel.beerservice.config;
 
 
+import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 
 @Configuration
 public class FeignClientSecurityConfig {
 
-    public BasicAuthenticationInterceptor basicAuthenticationInterceptor(
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(
             @Value("${mssc.brewery.beer-inventory-password}") String password,
             @Value("${mssc.brewery.beer-inventory-user}") String user) {
 
-        return new BasicAuthenticationInterceptor(user, password);
+        return new BasicAuthRequestInterceptor(user, password);
     }
 
 
