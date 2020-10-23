533e2260e864f904b60d5bcb678c2d1354c20450
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-23 17:54:27 +0200

    bootstrapdata

--- src/main/java/com/gabriel/beerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/gabriel/beerservice/bootstrap/BeerLoader.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +19,9 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,6 +37,7 @@ public class BeerLoader implements CommandLineRunner {
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_1_UPC)
+                    .id(BEER_1_UUID)
                     .price(new BigDecimal("12.95"))
                     .build());
 
@@ -42,6 +47,7 @@ public class BeerLoader implements CommandLineRunner {
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_2_UPC)
+                    .id(BEER_2_UUID)
                     .price(new BigDecimal("11.95"))
                     .build());
 
@@ -51,6 +57,7 @@ public class BeerLoader implements CommandLineRunner {
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_3_UPC)
+                    .id(BEER_3_UUID)
                     .price(new BigDecimal("11.95"))
                     .build());
         }
